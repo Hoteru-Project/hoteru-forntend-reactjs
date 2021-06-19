4923dc169b0884fddc62bf684b9dcfd20fb274de
Malik Hesham <malikhesham@outlook.com>
2021-06-20 00:46:50 +0200

    testing filter

--- src/components/Filter/NavbarComponent.jsx
+++ src/components/Filter/NavbarComponent.jsx
@@ -1,19 +1,94 @@
-import React, {Component} from "react";
-import AppBar from '@material-ui/core/AppBar';
+import React, {Component} from 'react'
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import { makeStyles } from '@material-ui/core/styles';
+import CheckboxList from './HotelsFeaturesComponent';
 
-class FilterNavbar extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div className="shadow-sm p-3 mt-5 rounded container" style={{"max-width":"80vw"}}>
-                <div className="aligner" style={{"display":"flex","alignItems":"center"}}>
-                    <nav className="navbar justify-content-between">
-                        <p className="navbar-brand"><strong>Sort by</strong> </p>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
-                    </nav>
-                </div>
-            </div>
-        );}
+// export default function BasicButtonGroup() {
+//   const classes = useStyles();
+
+//   return (
+    
+//     <div className={classes.root}>
+//       <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+//       <Button
+//         aria-controls="customized-menu"
+//         aria-haspopup="true"
+//         variant="contained"
+//         color="primary"
+//       >
+//         Open Menu
+//       </Button>
+//         <Button>Guest Rating</Button>
+//         <Button>Location</Button>
+//         <Button>More Filters</Button>
+//       </ButtonGroup>
+//     </div>
+//   );
+// }
+
+
+class NavbarComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+
+    fetch('http://127.0.0.1:8001/api/v1/hotels/test?checkIn=2021-06-07&checkOut=2021-06-08&location=alexandria&rooms=1&')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          <CheckboxList/>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
 }
- 
-export default FilterNavbar;
+
+export default NavbarComponent;
