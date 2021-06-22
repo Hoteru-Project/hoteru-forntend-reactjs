656d09a7cd9d0894a73c17bdf2b2dc9b98f4e20e
Malik Hesham <malikhesham@outlook.com>
2021-06-22 02:21:51 +0200

    adding newsletter components with style to be reviewed

--- src/components/Newsletter/NewsLetterComponent.jsx
+++ src/components/Newsletter/NewsLetterComponent.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe";
-import NewsLetterForm from './NewsLetterForm';
-// require( './mcFormStyles.scss');
+import { Link } from 'react-router-dom';
+import classes from "./NewsLetterComponent.css"
 
 
 const NewsLetterComponent = props => {
     const postUrl = `https://gmail.us6.list-manage.com/subscribe/post?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
     return (
-        <div className="mc__form-container">
-            <MailchimpSubscribe
-                url={postUrl}
-                render={({ subscribe, status, message }) => (
-                    <NewsLetterForm
-                        status={status} 
-                        message={message}
-                        onValidated={formData => subscribe(formData)}
-                    />
-                )}
-            />
+        <div className={classes.Container}>
+            <MailchimpSubscribe url={postUrl} />
         </div>
     );
 };
--- src/components/Newsletter/NewsLetterForm.jsx
+++ src/components/Newsletter/NewsLetterForm.jsx
@@ -2,32 +2,32 @@ import React, { useState, useEffect } from 'react';
 import MailchimpSubscribe from "react-mailchimp-subscribe"
 import TextField from '@material-ui/core/TextField';
 import { Button } from '@material-ui/core';
-import * as yup from "yup";
-import {​​​​​​​​yupResolver}​​​​​​​​ from "@hookform/resolvers/yup";
-import {​​​​​​​​useForm}​​​​​​​​ from "react-hook-form";
+// import * as yup from "yup";
+// import {​​​​​​​​yupResolver}​​​​​​​​ from "@hookform/resolvers/yup";
+// import {​​​​​​​​useForm}​​​​​​​​ from "react-hook-form";
 
 
-const schema = yup.object().shape({
-    email: yup.string().required().email()
-});
+// const schema = yup.object().shape({
+//     email: yup.string().required().email()
+// });
 
-const NewsLetterForm = ({ status, message, onValidated }) => {
-    const { register, formState: { errors, isValid }, handleSubmit } = useForm({ resolver: yupResolver(schema) });
+// const NewsLetterForm = ({ status, message, onValidated }) => {
+//     const { register, formState: { errors, isValid }, handleSubmit } = useForm({ resolver: yupResolver(schema) });
 
-    // const {modalOpen, setModalOpen} = useGHStContext();
+//     // const {modalOpen, setModalOpen} = useGHStContext();
 
-    const handleSubscribeSubmit = (data) => {
+//     const handleSubscribeSubmit = (data) => {
 
 
-    }
+//     }
 
-    return (
-        <form onSubmit={handleSubmit(handleSubscribeSubmit)}>
-            <TextField label="Email" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
-            <Button type="submit" disabled={!isValid}>subscribe</Button>
+//     return (
+//         <form onSubmit={handleSubmit(handleSubscribeSubmit)}>
+//             <TextField label="Email" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
+//             <Button type="submit" disabled={!isValid}>subscribe</Button>
 
-        </form>
-    );
-};
+//         </form>
+//     );
+// };
 
-export default NewsLetterForm;
+// export default NewsLetterForm;
